4b0da5821ce12ebf727d28ef328f988becd426c2
TyYip <124058314+TyYip@users.noreply.github.com>
2023-06-05 08:42:27 -0400

    6/5
    Trying to make shop values work. Creating all weapon and armor variables in player class

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from dungeon import Player, Mobs
 from shop import Shop
 from dungeon import Dungeon 
-from inventory import Weapons
-from inventory import Armors
-from inventory import Inventory
+# from inventory import Weapons
+# from inventory import Armors
+# from inventory import Inventory
 #initialize dungeon classes
 player = Player(100, 5, 0)
 skeleton = Mobs(30, 20, 2, 0)
 goblin = Mobs(50, 10, 2, 0)
 orc = Mobs(75, 5, 3, 0)
 
-inventory = Inventory(Weapons.in_use_weapon, 0, 0, 0, 0, Armors.in_use_armor)
-print("weapon:", inventory.weapon, "gold:", inventory.gold, "apple:", inventory.apple, "bread:", inventory.bread, "pie:", inventory.pie, "armor:", inventory.armor)
+# inventory = Inventory(Weapons.in_use_weapon, 0, 0, 0, 0, Armors.in_use_armor)
+# print("weapon:", inventory.weapon, "gold:", inventory.gold, "apple:", inventory.apple, "bread:", inventory.bread, "pie:", inventory.pie, "armor:", inventory.armor)
 
 weapons = []
 inventory_apple = 0
@@ -25,37 +25,11 @@ inventory_sledgehammer = 0
 inventory_axe = 0
 inventory_scythe = 0
 
-shop=Shop()
-shop.shop(player)
 which_map = input("Where do you want to travel? (Shop,Dungeon) ")
+continue_shop = input("Would you like to keep shopping? Y/N 💖 ")
 if which_map.capitalize() == "Shop":
-  what_buy = input("What do you want to buy? (Food,Armor,Weapon) ")
-  if what_buy.capitalize() == "Food":
-            which_food = input("which food? (Apple($5 ,20 heal)/Bread($20 ,50 heal)/Pie($50 ,100 heal)) ")
-            if which_food.capitalize() == "Apple":  
-               Shop.apple_buy()
-            if which_food.capitalize() == "Bread":
-               Shop.bread_buy()
-            if which_food.capitalize() == "Pie":  
-               Shop.pie_buy()
-  elif what_buy.capitalize() == "Armor":
-            which_armor = input("Which armor? (bronze($30, 20 hp)/iron($50, 35 hp)/steel($100, 50 hp)) ")
-            if which_armor.capitalize() == "Bronze":  
-               Shop.buy_bronze()
-            if which_armor.capitalize() == "Iron":  
-               Shop.buy_iron()
-            if which_armor.capitalize() == "Steel":  
-               Shop.buy_steel()
-  elif what_buy.capitalize() == "Weapon":
-            which_weapon = input("Which weapon? (sword($10, 15 dmg)/sledgehammer($20, 30 dmg)/axe($50, 50 dmg)/scythe(100$, 75 dmg) ")
-            if which_weapon.capitalize() == "Sword":
-               Shop.buy_sword()
-            if which_weapon.capitalize() == "Sledgehammer":
-               Shop.buy_sledge()
-            if which_weapon.capitalize() == "Axe":
-               Shop.buy_axe()
-            if which_weapon.capitalize() == "Scythe":
-               Shop.buy_scythe()
+      shop=Shop()
+      shop.shop(player)
 elif which_map.capitalize() == "Dungeon":
   print("iuasgdfuolhj")
 
--- shop.py
+++ shop.py
@@ -1,5 +1,3 @@
-
-weapons = []
 inventory_apple = 0
 inventory_bread = 0
 inventory_pie = 0
@@ -17,115 +15,114 @@ class Shop():
       player.gold = player.gold - (5 * int(how_much_apple))
       if player.gold >= 0:
          new_inventory_apple = int(inventory_apple) + int(how_much_apple)
-         print("Your gold", new_gold)
+         print("Your gold", player.gold)
          print("Your apple", new_inventory_apple)
-         return y
+         """ return y """
       else:
          print("Not enough gold")
-   def bread_buy():
+   def bread_buy(player):
       how_much_bread = input("How many bread do you want to buy? ")
-      new_gold = gold - (20 * int(how_much_bread))
-      if new_gold >= 0:
+      player.gold = player.gold - (20 * int(how_much_bread))
+      if player.gold >= 0:
          new_inventory_bread = int(inventory_bread) + int(how_much_bread)
-         print("Your gold", new_gold)
+         print("Your gold", player.gold)
          print("Your bread", new_inventory_bread)
       else:
          print("Not enough gold")
-   def pie_buy():
+   def pie_buy(player):
       how_much_pie = input("How many pie do you want to buy? ")
-      new_gold = gold - (50 * int(how_much_pie))
-      if new_gold >= 0:
+      player.gold = player.gold - (50 * int(how_much_pie))
+      if player.gold >= 0:
          new_inventory_pie = int(inventory_pie) + int(how_much_pie)
-         print("Your gold", new_gold)
+         print("Your gold", player.gold)
          print("Your pie", new_inventory_pie)
       else:
             print("Not enough gold")
-   def buy_bronze():
-      new_gold = gold - 30
-      if new_gold >= 0:
+   def buy_bronze(player, y):
+      player.gold = player.gold - 30
+      if player.gold >= 0:
          new_inventory_bronze = int(inventory_bronze) +1
-         print("Your gold", new_gold)
+         y.append("bronze")
+         print("Your gold", player.gold)
          print("Your bronze", new_inventory_bronze)
-   def buy_iron():
-      new_gold = gold - 50
-      if new_gold >= 0:
+   def buy_iron(player, y):
+      player.gold = player.gold - 50
+      if player.gold >= 0:
          new_inventory_iron = int(inventory_iron) + 1
-         print("Your gold", new_gold)
+         y.append("iron")
+         print("Your gold", player.gold)
          print("Your iron", new_inventory_iron)
-   def buy_steel():
-      new_gold = gold - 100
-      if new_gold >= 0:
+   def buy_steel(player, y):
+      player.gold = player.gold - 100
+      if player.gold >= 0:
          new_inventory_steel = int(inventory_steel) + 1
-         print("Your gold", new_gold)
+         y.append("steel")
+         print("Your gold", player.gold)
          print("Your steel", new_inventory_steel)
-   def buy_sword():
-      new_gold = gold["gold"] - 10
-      gold.update({'gold':new_gold})
-      if new_gold >= 0:
+   def buy_sword(player, x):
+      player.gold = player.gold - 10
+      if player.gold >= 0:
          new_inventory_sword = int(inventory_sword) + 1
-         weapons.append("sword")
-         print(weapons)
-         print("You gold",new_gold)
+         x.append("sword")
+         print("You gold",player.gold)
          print("Your sword",new_inventory_sword)
-   def buy_sledge():
-      new_gold = gold - 25
-      if new_gold >= 0:
+   def buy_sledge(player, x):
+      player.gold = player.gold - 25
+      if player.gold >= 0:
          new_inventory_sledgehammer = int(inventory_sledgehammer) + 1
-         weapons.append("sledgehammer")
-         print(weapons)
-         print("Your gold",new_gold)
+         x.append("sledgehammer")
+         print(x)
+         print("Your gold",player.gold)
          print("Your sledgehammer", new_inventory_sledgehammer)
-   def buy_axe():
-      new_gold = gold - 50
-      if new_gold >= 0:
+   def buy_axe(player, x):
+      player.gold = player.gold - 50
+      if player.gold >= 0:
          new_inventory_axe = inventory_axe + 1
-         weapons.append("axe")
-         print(weapons)
-         print("Your gold",new_gold)
+         x.append("axe")
+         print(x)
+         print("Your gold",player.gold)
          print("Your axe",new_inventory_axe)
-   def buy_scythe():
-      new_gold = gold - 100
-      if new_gold >= 0:
+   def buy_scythe(player, x):
+      player.gold = player.gold - 100
+      if player.gold >= 0:
          new_inventory_scythe = int(inventory_scythe) +1
-         weapons.append("scythe")
-         print(weapons)
-         print("Your gold",new_gold)
-         print("Your scythe",new_inventory_scythe)
-   
-   
+         x.append("scythe")
+         print(x)
+         print("Your gold",player.gold)
+         print("Your scythe",new_inventory_scythe)   
    def shop(self,x):
          what_buy = input("What do you want to buy? (Food,Armor,Weapon) ")
          if what_buy.capitalize() == "Food":
             which_food = input("which food? (Apple($5 ,20 heal)/Bread($20 ,50 heal)/Pie($50 ,100 heal)) ")
             if which_food.capitalize() == "Apple":  
-               apple_buy()
+               self.apple_buy()
             if which_food.capitalize() == "Bread":
-               bread_buy()
+               self.bread_buy()
             if which_food.capitalize() == "Pie":  
-               pie_buy()
+               self.pie_buy()
          elif what_buy.capitalize() == "Armor":
             which_armor = input("Which armor? (bronze($30, 20 hp)/iron($50, 35 hp)/steel($100, 50 hp)) ")
             if which_armor.capitalize() == "Bronze":  
-               buy_bronze()
+               self.buy_bronze()
             if which_armor.capitalize() == "Iron":  
-               buy_iron()
+               self.buy_iron()
             if which_armor.capitalize() == "Steel":  
-               buy_steel()
+               self.buy_steel()
          elif what_buy.capitalize() == "Weapon":
             which_weapon = input("Which weapon? (sword($10, 15 dmg)/sledgehammer($20, 30 dmg)/axe($50, 50 dmg)/scythe(100$, 75 dmg) ")
             if which_weapon.capitalize() == "Sword":
-               buy_sword()
+               self.buy_sword()
             if which_weapon.capitalize() == "Sledgehammer":
-               buy_sledge()
+               self.buy_sledge()
             if which_weapon.capitalize() == "Axe":
-               buy_axe()
+               self.buy_axe()
             if which_weapon.capitalize() == "Scythe":
-               buy_scythe()
-continue_shop = input("Would you like to keep shopping? Y/N 💖 ")
-while continue_shop.upper() == "Y":
-   if continue_shop.upper() == 'Y':
+               self.buy_scythe()
+# continue_shop = input("Would you like to keep shopping? Y/N 💖 ")
+# while continue_shop.upper() == "Y":
+#    if continue_shop.upper() == 'Y':
       
-    continue_shop = input("Would you like to keep shopping? Y/N 💖 ")
+#     continue_shop = input("Would you like to keep shopping? Y/N 💖 ")
 
 
 
